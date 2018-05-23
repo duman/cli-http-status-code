7a73e3941c84528025c133e85099e56af24d0ce7
Tuğberk Kaan Duman <kaan@dumanmail.com>
2018-05-23 15:31:42 +0300

    replace_url is now replace_str in duman.h

--- google-lucky.cpp
+++ google-lucky.cpp
@@ -1,7 +1,5 @@
 #include <duman.h> 
 
-void replace_url(string&, const string&, const string&);
-
 int main(int argc, char* argv[])
 {
 	string base_url = "https://www.google.com/search?q=";
@@ -18,18 +16,8 @@ int main(int argc, char* argv[])
 	}
 
 	base_url += query;
-	replace_url(base_url, "\"", "\\\"");
-	replace_url(base_url, "+", "%2B");
+	replace_str(base_url, "\"", "\\\"");
+	replace_str(base_url, "+", "%2B");
 	base_url += "&btnI"; // feeling lucky button
 	open_url(base_url.c_str());
-}
-
-void replace_url(string& str, const string& from, const string& to) {
-    if(from.empty())
-        return;
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length();
-    }
 }
