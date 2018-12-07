f9a0f850dd348e2d19b4aa069388e55d1b2f2bbd
Tuğberk Kaan Duman <kaan@dumanmail.com>
2018-12-07 23:24:03 +0300

    Improved way of escaping argv[0]

--- google-lucky.cpp
+++ google-lucky.cpp
@@ -1,21 +1,26 @@
-#include <duman.h> 
+#include "duman.h"
 
 int main(int argc, char* argv[])
 {
 	string base_url = "https://www.google.com/search?q=";
 
-	auto query = GetCommandLine();
-
-	const auto l = strlen(argv[0]);
-
-	if (query == strstr(query, argv[0]))
-	{
-		query = query + l;
-		while (*query && isspace(*query))
+	char *query = GetCommandLine();
+	if (*query == '"') {
+		++query;
+		while (*query)
+			if (*query++ == '"')
+				break;
+	}
+	else {
+		while (*query && *query != ' ' && *query != '\t')
 			++query;
 	}
 
+	while (*query == ' ' || *query == '\t')
+		query++;
+
 	base_url += query;
+	replace_str(base_url, " ", "%20");
 	replace_str(base_url, "\"", "\\\"");
 	replace_str(base_url, "+", "%2B");
 	base_url += "&btnI"; // feeling lucky button
